b382fd34c510b233cc6c3224fd42c831015d9516
gauravgjadhav <92154289+gauravgjadhav@users.noreply.github.com>
2022-07-09 22:30:48 +0530

    Add files via upload

---
+++ UserNameApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        CaseStudy();
+    }
+
+    private static void CaseStudy()
+    {
+        string Path = (@"F:\SwabhavTraningRepositiry\Csharp\Basics\oop\NotOverriteText\NotOverriteText\Information.txt");
+        StreamWriter username = File.AppendText(Path);
+        Console.WriteLine("Enter the user name");
+        string UserName = Console.ReadLine();
+        username.WriteLine(UserName);
+        username.Close();
+    }
+}
